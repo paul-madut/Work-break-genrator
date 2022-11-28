488ca35d029d83d91ac74711537600dee06aa60b
paul-madut <paul.madut@mths.ca>
2022-11-28 16:22:50 -0500

    added twilio api

--- FarmboySchedule/main.py
+++ FarmboySchedule/main.py
@@ -1,6 +1,13 @@
 # Code Written by Paul Madut and is not to be used by any other entity
 
 import datetime
+from twilio.rest import Client
+
+account_sid = 'AC48f54b1fc2e0f214851b67e491c1be88'
+auth_token = 'b15bdafe418fc6260adac9ae12be2ba5'
+client = Client(account_sid, auth_token)
+
+
 class Worker:
     def __init__(self, name, startTime, endTime):
         self.name = name
@@ -230,6 +237,13 @@ while(flag):
 
 for x in WorkingToday2:
     x.MakePretty()
-    print(x)
+    message = client.messages.create(
+        messaging_service_sid='MG835442ee15df88dd2018d4a8c1c04b29',
+        body=x,
+        to='+16139814476'
+    )
+
+print("done")
+
 
 
